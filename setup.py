d3c7b03587432ae453c1191fb3713e8b384c5185
Jan Benda <jan.benda@uni-tuebingen.de>
2016-06-01 20:13:38 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='audioio',
-      version='0.1',
+      version='0.2',
       packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
       description='Platform independent reading of audio files as well as recording and
 playing of audio data.',
       author='Jan Benda, Joerg Henninger',
-      requires=['numpy']
+      requires=['numpy', 'pysoundfile', 'audioread', 'pyaudio']
       )
