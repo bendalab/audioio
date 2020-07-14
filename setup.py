ffc2eed6784d64359b3860722581a7a1af885b43
Jan Benda <jan.benda@uni-tuebingen.de>
2020-07-14 23:31:14 +0200

    [setup] updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 exec(open('audioio/version.py').read())
 
-setup(name='audioio',
-      version=__version__,
-      packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
-      entry_points={
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='audioio',
+    version=__version__,
+    author='Jan Benda, Joerg Henninger',
+    author_email="jan.benda@uni-tuebingen.de",
+    description='Platform independent reading of audio files as well as recording and playing of audio data.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bendalab/audioio",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
+    entry_points={
         'console_scripts': [
             'audioconverter = audioio.audioconverter:main',
         ]},
-      description='Platform independent reading of audio files as well as recording and playing of audio data.',
-      author='Jan Benda, Joerg Henninger',
-      requires=['numpy', 'pysoundfile', 'audioread', 'pyaudio']
-      )
+    requires=['numpy', 'pysoundfile', 'audioread', 'pyaudio']
+)
