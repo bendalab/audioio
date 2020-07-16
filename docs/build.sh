22229df344555cdb874cc07ff1229f2464e55f17
Jan Benda <jan.benda@uni-tuebingen.de>
2020-07-16 18:59:54 +0200

    [travis] doc deployment

---
+++ docs/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+die () { echo "ERROR: $*" >&2; exit 2; }
+
+for cmd in pdoc3; do
+    command -v "$cmd" >/dev/null ||
+        die "Missing $cmd; \`pip install $cmd\`"
+done
+
+PACKAGE="audioio"
+DOCROOT="$(dirname "$(realpath "$0")")"
+BUILDROOT="$DOCROOT/build"
+
+echo
+echo "Building API reference docs for $PACKAGE"
+echo
+
+mkdir -p "$BUILDROOT"
+rm -r "$BUILDROOT/doc" 2> /dev/null || true
+cd "$DOCROOT/.."
+pdoc3 --html --output-dir "$BUILDROOT" $PACKAGE
+mv $BUILDROOT/$PACKAGE $BUILDROOT/doc
+cd - > /dev/null
+
+echo
+echo "Done. Docs in:"
+echo
+echo "    file://$BUILDROOT/doc/index.html"
+echo
