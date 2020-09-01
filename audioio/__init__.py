6ec3692fc4678dcce78018b776103c41ced5c701
Jan Benda <jan.benda@uni-tuebingen.de>
2020-09-01 23:34:23 +0200

    [audioloader] new blocks() generator

--- audioio/__init__.py
+++ audioio/__init__.py
@@ -15,7 +15,7 @@ from audioio.audiomodules import available_modules, unavailable_modules
 from audioio.audiomodules import disable_module, enable_module, select_module
 from audioio.audiomodules import missing_modules, missing_modules_instructions
 from audioio.audiomodules import installation_instruction
-from audioio.audioloader import load_audio, unwrap, AudioLoader
+from audioio.audioloader import load_audio, AudioLoader, blocks, unwrap
 from audioio.audiowriter import write_audio, available_formats, available_encodings
 from audioio.playaudio import play, beep, PlayAudio
 from audioio.playaudio import note2freq, fade_in, fade_out, fade
