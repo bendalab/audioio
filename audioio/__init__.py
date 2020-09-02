3bb91f5729aa52b57a514cded6c02336c041124d
Jan Benda <jan.benda@uni-tuebingen.de>
2020-09-02 22:19:38 +0200

    avoid double inclusion of audioio modules

--- audioio/__init__.py
+++ audioio/__init__.py
@@ -2,21 +2,26 @@
 Platform independent interfacing of numpy arrays with audio files and devices.
 """
 
-from .version import __version__
+import sys
 
-__all__ = ['audiomodules',
-           'audioloader',
-           'audiowriter',
-           'playaudio']
-
-# make all important functions available in the audioio namespace:
-from audioio.audiomodules import list_modules, installed_modules
-from audioio.audiomodules import available_modules, unavailable_modules
-from audioio.audiomodules import disable_module, enable_module, select_module
-from audioio.audiomodules import missing_modules, missing_modules_instructions
-from audioio.audiomodules import installation_instruction
-from audioio.audioloader import load_audio, AudioLoader, blocks, unwrap
-from audioio.audiowriter import write_audio, available_formats, available_encodings
-from audioio.playaudio import play, beep, PlayAudio
-from audioio.playaudio import note2freq, fade_in, fade_out, fade
-
+# avoid double inclusion of audioio modules if called as modules,
+# e.g. python -m audioio.audiowriter`:
+if not '-m' in sys.argv:
+    
+    from .version import __version__
+    
+    __all__ = ['audiomodules',
+               'audioloader',
+               'audiowriter',
+               'playaudio']
+    
+    # make all important functions available in the audioio namespace:
+    from .audiomodules import list_modules, installed_modules
+    from .audiomodules import available_modules, unavailable_modules
+    from .audiomodules import disable_module, enable_module, select_module
+    from .audiomodules import missing_modules, missing_modules_instructions
+    from .audiomodules import installation_instruction
+    from .audioloader import load_audio, AudioLoader, blocks, unwrap
+    from .audiowriter import write_audio, available_formats, available_encodings
+    from .playaudio import play, beep, PlayAudio
+    from .playaudio import note2freq, fade_in, fade_out, fade
