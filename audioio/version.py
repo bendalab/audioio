49cf80cd9246e3b9ea82114acd48b7c625ec8f2b
Jan Benda <jan.benda@uni-tuebingen.de>
2022-04-12 21:51:58 +0200

    [version] update

--- audioio/version.py
+++ audioio/version.py
@@ -1,15 +1,13 @@
 """ Version and year of the audioio package.
 """
 
+__version__ = '0.8.0'
+""" Current version of the audioio package. """
+
+__year__ = '2022'
+""" Year of the current audioio version. """
 
 __pdoc__ = {}
 __pdoc__['__version__'] = True
 __pdoc__['__year__'] = True
-
-
-__version__ = '0.10.0'
-""" Current version of the audioio package. """
-
-__year__ = '2020'
-""" Year of the current audioio version. """
 
