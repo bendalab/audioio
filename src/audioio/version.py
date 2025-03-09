184c6f3b59b582d29c048aec03cc1cba5b48b01e
Jan Benda <jan.benda@uni-tuebingen.de>
2025-03-09 10:42:10 +0100

    [version] new version 2.4.0

--- src/audioio/version.py
+++ src/audioio/version.py
@@ -1,7 +1,7 @@
 """ Version and year of the audioio package.
 """
 
-__version__ = '2.3.0'
+__version__ = '2.4.0'
 """ Current version of the audioio package as string 'x.y.z'. """
 
 __year__ = '2025'
